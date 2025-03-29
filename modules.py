fbd01a758f499aea5fd603cf7295c34c9b1d4bf1
Nauman Irshad Ali Shah <shahjee3565@gmail.com>
2025-03-29 21:03:16 +0500

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,16 @@
+import modules  
+from modules import greet, pi 
+import modules as mm
+import math
+import random
+print(mm.pi+9)
+
+print(pi+3) 
+print(modules.pi*33)
+
+
+print(math.sqrt(25))
+print(math.factorial(5))  
+
+print(random.randint(1,5))
+
